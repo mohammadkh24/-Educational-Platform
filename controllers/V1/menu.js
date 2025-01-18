0e5fd24ca457b8b02da94ccdc575f5c20ba0ba12
Mohammad <mohammadgithub2024@gmail.com>
2025-01-18 14:04:31 +0330

    add features : create for menu controller

--- controllers/V1/menu.js
+++ controllers/V1/menu.js
@@ -1,19 +1,34 @@
-const menusModel = require("../../models/menu")
+const { param } = require("../../app");
+const menusModel = require("../../models/menu");
 
-exports.getAll = async (req, res) => {
-    
-};
+exports.getAll = async (req, res) => {};
 exports.create = async (req, res) => {
-    const {title , href , parent} = req.body;
+  try {
+    const { title, href, parent } = req.body;
 
-    
-};
-exports.getAllInPannel = async (req, res) => {
+    const isParent = await menusModel.findOne({ _id : parent});
 
-};
-exports.remove = async (req, res) => {
+    if (parent && !isParent) {
+       return res.status(404).json({
+            message : "Parent not found !!"
+        })
+    }
 
-};
-exports.update = async (req, res) => {
+    const menu = await menusModel.create({
+      title,
+      href,
+      parent,
+    });
 
+    return res.status(201).json({
+      message: "Menu created successfully",
+      menu,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
+
+exports.getAllInPannel = async (req, res) => {};
+exports.remove = async (req, res) => {};
+exports.update = async (req, res) => {};

5687aa385dee3ed84e63357e88f21d746b86bc3a
Mohammad <mohammadgithub2024@gmail.com>
2025-01-18 15:13:10 +0330

    add features : getAll for menu controller

--- controllers/V1/menu.js
+++ controllers/V1/menu.js
@@ -1,7 +1,24 @@
-const { default: mongoose, Types } = require("mongoose");
 const menusModel = require("../../models/menu");
+const { default: mongoose, Types } = require("mongoose");
 
-exports.getAll = async (req, res) => {};
+exports.getAll = async (req, res) => {
+  const menus = await menusModel.find({}).lean();
+
+  menus.forEach((menu) => {
+    const subMenus = [];
+    for (let i = 0; i < menus.length; i++) {
+      const mainMenu = menus[i];
+      if (String(mainMenu.parent) === String(menu._id)) {
+        subMenus.push(menus.splice(i, 1)[0]);
+        i = i - 1;
+      }
+    }
+    menu.subMenus = subMenus;
+  });
+
+  return res.json(menus);
+};
+
 exports.create = async (req, res) => {
   try {
     const { title, href, parent } = req.body;
@@ -59,37 +76,40 @@ exports.remove = async (req, res) => {
 };
 
 exports.update = async (req, res) => {
-    const {id} = req.params;
-    const {title , href , parent} = req.body;
+  const { id } = req.params;
+  const { title, href, parent } = req.body;
 
-    const isParent = await menusModel.findOne({ _id: parent });
+  if (!Types.ObjectId.isValid(id)) {
+    return res.status(400).json({
+      message: "MenuID is not valid !!",
+    });
+  }
 
-    if (parent && !isParent) {
-      return res.status(404).json({
-        message: "Parent not found !!",
-      });
+  const isParent = await menusModel.findOne({ _id: parent });
+
+  if (parent && !isParent) {
+    return res.status(404).json({
+      message: "Parent not found !!",
+    });
+  }
+
+  const updateMenu = await menusModel.findOneAndUpdate(
+    { _id: id },
+    {
+      title,
+      href,
+      parent,
     }
+  );
 
-    if (!Types.ObjectId.isValid(id)) {
-        return res.status(400).json({
-          message: "MenuID is not valid !!",
-        });
-      }
+  if (!updateMenu) {
+    return res.status(404).json({
+      message: "Menu not found !!",
+    });
+  }
 
-      const updateMenu = await menusModel.findOneAndUpdate({ _id: id } , {
-        title,
-        href,
-        parent
-      });
-
-      if (!updateMenu) {
-        return res.status(404).json({
-          message: "Menu not found !!",
-        });
-      }
-
-      return res.status(201).json({
-        message : "Menu updated successfully",
-        updateMenu
-      })
+  return res.status(201).json({
+    message: "Menu updated successfully",
+    updateMenu,
+  });
 };
